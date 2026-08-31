6d5b3e5bf6848e20e9902af2ae64290b2f94c25a
leigcsn-sys <jlgacusana@gmail.com>
2026-09-01 00:40:44 +0800

    style: update navigation header branding and user menu treatment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,8 +16,8 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+    <body class="font-sans antialiased bg-[#5f7483]">
+        <div class="min-h-screen bg-[#5f7483]">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
